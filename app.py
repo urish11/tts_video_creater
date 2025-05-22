387a39f06a29cd2aa415bbf125a0fdab7e0cb7be
urish11 <117926571+urish11@users.noreply.github.com>
2025-05-22 23:07:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -441,7 +441,9 @@ def create_video_with_image_on_top(media_assets, topic, progress_bar=None):
                 text_clips.append(txt_clip)
             
             # Combine image, audio, and timed subtitles
-            video = CompositeVideoClip([img_clip] + text_clips, size=(1080, 1920)).set_audio(audio_clip).set_duration(duration)
+            # video = CompositeVideoClip([img_clip] + text_clips, size=(1080, 1920)).set_audio(audio_clip).set_duration(duration)
+            video = CompositeVideoClip([img_clip] + text_clips, size=(1080, 1920)).set_audio(audio_clip.set_duration(duration))
+
             clips.append(video)
             
             # Update progress
@@ -472,7 +474,8 @@ def create_video_with_image_on_top(media_assets, topic, progress_bar=None):
         final_audio = CompositeAudioClip([final_video.audio, music_audio_clip])
         
         # Set the final audio to the video
-        ########################################final_video = final_video.set_audio(final_audio)
+        ########################################
+        final_video = final_video.set_audio(final_audio)
 
 
         file_name = f"output_video_{urllib.parse.quote(topic.replace(' ', '_')[:40], safe='')}_{int(datetime.datetime.now().timestamp())}.mp4".replace("'",'').replace('"','')
