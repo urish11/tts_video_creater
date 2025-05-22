df878123b3aa37817ed5a69a859f2f5a2289fbdb
urish11 <117926571+urish11@users.noreply.github.com>
2025-05-22 22:38:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -327,7 +327,7 @@ def generate_audio_with_timestamps(text, client, voice_id="alloy"):
         model="generate_audio_with_timestamps",
         voice=openai_voice,
         input=text,
-        instructions=instructions_per_voice[openai_voice]['instructions']
+        instructions=instructions_per_voice[openai_voice]['instructions'],
         response_format="mp3",
         speed=1.10
     )
