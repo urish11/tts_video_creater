0e1cb543e95fa576dacd8818176f848d859a5736
urish11 <117926571+urish11@users.noreply.github.com>
2025-05-28 12:58:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -791,7 +791,7 @@ if st.button("Generate Videos"):
                                 • Every image MUST contain a **fully self-contained description of the person/avatar** (e.g. “40s Black man in casual gym clothes, sweating and smiling after a workout”), even if they appear in multiple slides.
                                 • Use **simple**, engaging, curiosity-driven text. Avoid buzzwords, fake promises, or corporate tone. but still be very convincing and eye catching!!!
                                 • End with a strong convinsing  CTA like: “Click to explore options” or “Tap to see how it works.”
-                                • showing a {gender} for appropriate {age}\\look\\{race}\etc in images!!!
+                                • showing a {gender}  {age}\\look\\{race}\ in images!!!
                                 • end with a positive outcome visual, sell the dream : like if for a degree, someone holding the diploma proudly , or if for a car, someone in a brand new luxury car happy
 
                                 REALLY SELL IT FROM THE GET GO!
