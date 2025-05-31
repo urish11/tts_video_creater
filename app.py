9bdc821847e65bcd86eec59cc97c5c70776ed56d
urish11 <117926571+urish11@users.noreply.github.com>
2025-05-31 22:12:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -817,7 +817,7 @@ if st.button("Generate Videos"):
                                 • Use **simple**, engaging, curiosity-driven text. Avoid buzzwords, fake promises, or corporate tone. but still be very convincing and eye catching!!!
                                 • End with a strong convinsing  CTA like: “Click to explore options” or “Tap to see how it works.”
                                 • showing a {gender}  {age}\\look\\{race}\ in images!!!
-                                •End with a powerful visual that proudly showcases the outcome to the camera — like someone beaming as they hold up their diploma or point at their new luxury car. Make it feel like they’re saying, ‘This could be you,’ urging the viewer to act
+                                •End with a powerful visual that proudly showcases the outcome to the camera — like someone beaming as they hold up their diploma or point at their new luxury car. Make it feel like they’re saying, ‘This could be you,’ urging the viewer to act. you can make them point to the topic!
                                 REALLY SELL IT FROM THE GET GO!
                                 Avatar:
                                 - Choose and show one realistic human avatar (gender, age, race, clothing style, mood, etc.)
